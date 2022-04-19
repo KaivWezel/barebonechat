7e0e59d3b5e3fcb2c709505e3aa06693f91929c7
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-04-19 11:55:21 +0200

    added public folder route

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs").set("views", path.join(__dirname + "/views"));
 
+app.use(express.static("public"));
+
 io.on("connection", (socket) => {
-	console.log("user connected", socket);
+	console.log("user connected");
 });
 
 server.listen(port, () => {
