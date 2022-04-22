3b476e026240561c3593aa272a324b4ce8ecc555
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-04-22 17:39:41 +0200

    Finished server, still fixing peerjs

--- server.js
+++ server.js
@@ -6,29 +6,47 @@ const { v4: uuidv4 } = require("uuid");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, { path: "/", debug: true });
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs").set("views", path.join(__dirname + "/views"));
 
-app.use(express.static("public"));
-
-io.on("connection", (socket) => {
-	socket.on("join room", (roomId, userId) => {
-		socket.join(roomId);
-	});
-	socket.on("message", (msg) => {
-		socket.emit("message", msg);
-	});
-});
+app.use(express.static("public")).use(express.urlencoded());
+app.use("/peerjs", peerServer);
 
 server.listen(port, () => {
-	console.log("listening to port:" + port);
+	console.log("listening to port: " + port);
 });
 
 app.get("/", (req, res) => {
-	res.redirect(`/${uuidv4()}`);
+	res.render("index");
+});
+
+app.post("/create-room", (req, res) => {
+	console.log(req.body);
+	res.redirect(`/${req.body.roomId}`);
+});
+
+app.post("/join-room", (req, res) => {
+	console.log(req.body);
+	res.redirect(`/${req.body.room}`);
 });
 
 app.get("/:room", (req, res) => {
-	res.render("room");
+	res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+	console.log("user connected");
+	socket.on("join-room", (roomId, userId) => {
+		socket.join(roomId);
+		socket.to(roomId).emit("user-connected", userId);
+	});
+	socket.on("chat-message", (roomId, msg) => {
+		io.to(roomId).emit("chat-message", msg);
+	});
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
 });
